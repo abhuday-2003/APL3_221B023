e5cff342f881f5b101ef6b9a9d1d176c305211b8
221B023 <221B023@juet.co.in>
2025-04-21 14:17:35 +0530

    commit

---
+++ le6(Decorator)/Coffee.java
@@ -0,0 +1,11 @@
+class Coffee extends Offering
+{
+    int getPrice()
+    {
+        return 30;
+    }
+    String getName()
+    {
+        return "Coffee";
+    }
+}
---
+++ le6(Decorator)/Tea.java
@@ -0,0 +1,11 @@
+class Tea extends Offering
+{
+     int getPrice()
+    {
+        return 20;
+    }
+    String getName()
+    {
+        return "Tea";
+    }
+}
